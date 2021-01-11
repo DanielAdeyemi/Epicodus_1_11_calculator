7b915665a77a727dd77864d46885e76697b7874b
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-11 14:13:20 -0800

    finish basic ariphmetic functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,22 @@ function add(number1, number2) {
   return result;
 }
 
-function divide(number1, number2) {
+function substract(number1, number2) {
   let result = number1 - number2;
+  return result;
+}
+
+function divide(number1, number2) {
+  let result = number1 / number2;
+  return result;
+}
+
+function multiply(number1, number2) {
+  let result = number1 * number2;
   return result;
 }
 
 let number1 = parseFloat(prompt("Enter a number: "));
 let number2 = parseFloat(prompt("Enter another number: "));
 
-alert(divide(number1, number2));
+alert(multiply(number1, number2));
